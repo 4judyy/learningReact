30c48534c9776974407cec5375985d3790466734
Judy Zhou <dzhou224@uwo.ca>
2023-01-01 02:36:39 -0500

    handling page changes (pagination is very cool)

--- vidly/src/components /movies.jsx
+++ vidly/src/components /movies.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { getMovies } from "../services/fakeMovieService";
 import { deleteMovie } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
 
 class Movies extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        currentPage: 1,
+        pageSize: 4,
     }
 
     handleDelete = (id) => {
@@ -20,12 +23,19 @@ class Movies extends Component {
         this.setState({ movies });
     }
 
+    handlePageChange = page => {
+        this.setState({ currentPage: page });
+    }
+
     render() {
 
-        if (this.state.movies.length === 0) return <p>There are no movies</p>;
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage } = this.state;
 
-        return <div>
-            <p>Showing {this.state.movies.length} movies in the database</p>
+        if (count === 0) return <p>There are no movies</p>;
+
+        return <React.Fragment>
+            <p>Showing {count} movies in the database</p>
             <table className="table">
                 <thead>
                     <tr>
@@ -51,7 +61,12 @@ class Movies extends Component {
                     }
                 </tbody>
             </table >
-        </div>
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={this.handlePageChange} />
+        </React.Fragment>
     }
 }
 
